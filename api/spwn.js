81b6573a7ce19c0e4d034204281ed1cee2cd469d
sakapariwisatanasional <sakapariwisatanasional@gmail.com>
2026-09-25 15:01:45 +0700

    Update spwn.js

--- api/spwn.js
+++ api/spwn.js
@@ -1,23 +1,27 @@
+
+    
+  
 /**
  * SPWN Apps 2.0
  * Vercel API Proxy Gateway
+ *
+ * Perbaikan:
+ * - Menangani response GAS yang bukan JSON
+ * - Menampilkan error backend asli
+ * - Mencegah JSON parse crash
  */
 
-
 export default async function handler(req, res) {
-
 
   res.setHeader(
     "Access-Control-Allow-Origin",
     "*"
   );
 
-
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS"
   );
-
 
   res.setHeader(
     "Access-Control-Allow-Headers",
@@ -25,111 +29,72 @@ export default async function handler(req, res) {
   );
 
 
-
   if (req.method === "OPTIONS") {
 
     return res.status(200).json({
-      success:true
+      success: true
     });
 
   }
 
 
-
   try {
 
-
     const SPWN_SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbzuR8k2KbXHb6om2eNaIGM3yBBBsZtEFoLKji1H2dAWp4a6v8nrBAbwQj_S5S-SPBtXOg/exec";
+      "https://script.google.com/macros/s/AKfycbzuR8k2KbXHb6om2eNaIGM3yBBBsZtEFoLKji1H2dAWp4a6v8nrBAbwQj_S5S-SPBtXOg/exec";
 
 
-const url =
-  new URL(
-    SPWN_SCRIPT_URL
-  );
+    const url =
+      new URL(SPWN_SCRIPT_URL);
 
-
-
-    // teruskan query parameter
 
     Object.entries(
       req.query || {}
     ).forEach(
-      ([key,value]) => {
+      ([key, value]) => {
 
         url.searchParams.set(
           key,
-          value
+          String(value)
         );
 
       }
     );
 
 
-
-    let body = undefined;
-
+    let body;
 
 
-    if(req.method === "POST") {
+    if (req.method === "POST") {
 
       body =
         JSON.stringify(
-          req.body
+          req.body || {}
         );
 
     }
 
 
-
     const forwardHeaders = {
-      "Content-Type": "application/json"
+
+      "Content-Type":
+        "application/json"
+
     };
-    if (req.headers && req.headers.authorization) {
-      forwardHeaders["Authorization"] = req.headers.authorization;
+
+
+    if (
+      req.headers &&
+      req.headers.authorization
+    ) {
+
+      forwardHeaders["Authorization"] =
+        req.headers.authorization;
+
     }
+
 
     const response =
       await fetch(
         url.toString(),
-        {
-          method: req.method,
-          headers: forwardHeaders,
-          body
-        }
-      );
 
-
-
-    const data =
-      await response.json();
-
-
-
-    return res
-      .status(200)
-      .json(data);
-
-
-
-  } catch(error){
-
-
-    return res
-      .status(500)
-      .json({
-
-        success:false,
-
-        error_code:
-          "PROXY_ERROR",
-
-        message:
-          error.message
-
-      });
-
-
-  }
-
-}
